824b372f1053ecb0bf9af05baee07417168df3c2
SwaggyD761 <70922045+SwaggyD761@users.noreply.github.com>
2023-10-11 14:49:18 +0200

    Create homeStyles.css

---
+++ site/Home/homeStyles.php
@@ -0,0 +1,357 @@
+.register,
+.register * {
+  box-sizing: border-box;
+}
+.register {
+  background: #ffffff;
+  width: 1440px;
+  height: 1024px;
+  position: relative;
+  overflow: hidden;
+}
+.footer {
+  border-style: solid;
+  border-color: #000000;
+  border-width: 1px;
+  width: 1440px;
+  height: 77px;
+  position: absolute;
+  left: 0px;
+  top: 947px;
+  overflow: hidden;
+}
+.banner-ad-display {
+  border-style: solid;
+  border-color: #000000;
+  border-width: 1px;
+  width: 501px;
+  height: 77px;
+  position: absolute;
+  left: 939px;
+  top: 0px;
+  overflow: hidden;
+}
+.baner-ad-display {
+  border-style: solid;
+  border-color: #000000;
+  border-width: 1px;
+  width: 501px;
+  height: 76px;
+  position: absolute;
+  left: 0px;
+  top: 0px;
+  overflow: hidden;
+}
+.company-title {
+  border-style: solid;
+  border-color: #000000;
+  border-width: 1px;
+  width: 349px;
+  height: 46px;
+  position: absolute;
+  left: 546px;
+  top: 15px;
+  overflow: hidden;
+}
+.register-form-container {
+  border-style: solid;
+  border-color: #000000;
+  border-width: 1px;
+  width: 783px;
+  height: 662px;
+  position: absolute;
+  left: 329px;
+  top: 181px;
+  overflow: hidden;
+}
+.submit-button {
+  border-style: solid;
+  border-color: #000000;
+  border-width: 1px;
+  width: 204px;
+  height: 58px;
+  position: absolute;
+  left: 470px;
+  top: 577px;
+  overflow: hidden;
+}
+.clear-button {
+  border-style: solid;
+  border-color: #000000;
+  border-width: 1px;
+  width: 204px;
+  height: 58px;
+  position: absolute;
+  left: 111px;
+  top: 577px;
+  overflow: hidden;
+}
+.register-form {
+  border-style: solid;
+  border-color: #000000;
+  border-width: 1px;
+  width: 711px;
+  height: 528px;
+  position: absolute;
+  left: 36px;
+  top: 34px;
+  overflow: hidden;
+}
+.form-header {
+  border-style: solid;
+  border-color: #000000;
+  border-width: 1px;
+  width: 281px;
+  height: 42px;
+  position: absolute;
+  left: 215px;
+  top: 10px;
+  overflow: hidden;
+}
+.password-entry {
+  width: 565px;
+  height: 42px;
+  position: absolute;
+  left: 73px;
+  top: 346px;
+}
+.password-input {
+  border-style: solid;
+  border-color: #000000;
+  border-width: 1px;
+  width: 317px;
+  height: 42px;
+  position: absolute;
+  left: 248px;
+  top: 0px;
+  overflow: hidden;
+}
+.password-label {
+  border-style: solid;
+  border-color: #000000;
+  border-width: 1px;
+  width: 199px;
+  height: 42px;
+  position: absolute;
+  left: 0px;
+  top: 0px;
+  overflow: hidden;
+}
+.confirm-password-entry {
+  width: 565px;
+  height: 42px;
+  position: absolute;
+  left: 73px;
+  top: 402px;
+}
+.confirm-password-input {
+  border-style: solid;
+  border-color: #000000;
+  border-width: 1px;
+  width: 317px;
+  height: 42px;
+  position: absolute;
+  left: 248px;
+  top: 0px;
+  overflow: hidden;
+}
+.confirm-password-label {
+  border-style: solid;
+  border-color: #000000;
+  border-width: 1px;
+  width: 199px;
+  height: 42px;
+  position: absolute;
+  left: 0px;
+  top: 0px;
+  overflow: hidden;
+}
+.surname-entry {
+  width: 565px;
+  height: 42px;
+  position: absolute;
+  left: 73px;
+  top: 178px;
+}
+.surname-input {
+  border-style: solid;
+  border-color: #000000;
+  border-width: 1px;
+  width: 317px;
+  height: 42px;
+  position: absolute;
+  left: 248px;
+  top: 0px;
+  overflow: hidden;
+}
+.surname-label {
+  border-style: solid;
+  border-color: #000000;
+  border-width: 1px;
+  width: 199px;
+  height: 42px;
+  position: absolute;
+  left: 0px;
+  top: 0px;
+  overflow: hidden;
+}
+.already-have-an-account {
+  border-style: solid;
+  border-color: #000000;
+  border-width: 1px;
+  width: 248px;
+  height: 42px;
+  position: absolute;
+  left: 97px;
+  top: 458px;
+  overflow: hidden;
+}
+.register-with-google {
+  border-style: solid;
+  border-color: #000000;
+  border-width: 1px;
+  width: 248px;
+  height: 42px;
+  position: absolute;
+  left: 365px;
+  top: 458px;
+  overflow: hidden;
+}
+.cell-number-entry {
+  width: 565px;
+  height: 42px;
+  position: absolute;
+  left: 73px;
+  top: 290px;
+}
+.cell-number-input {
+  border-style: solid;
+  border-color: #000000;
+  border-width: 1px;
+  width: 317px;
+  height: 42px;
+  position: absolute;
+  left: 248px;
+  top: 0px;
+  overflow: hidden;
+}
+.cell-number-label {
+  border-style: solid;
+  border-color: #000000;
+  border-width: 1px;
+  width: 199px;
+  height: 42px;
+  position: absolute;
+  left: 0px;
+  top: 0px;
+  overflow: hidden;
+}
+.name-entry {
+  width: 565px;
+  height: 42px;
+  position: absolute;
+  left: 73px;
+  top: 122px;
+}
+.name-input {
+  border-style: solid;
+  border-color: #000000;
+  border-width: 1px;
+  width: 317px;
+  height: 42px;
+  position: absolute;
+  left: 248px;
+  top: 0px;
+  overflow: hidden;
+}
+.name-label {
+  border-style: solid;
+  border-color: #000000;
+  border-width: 1px;
+  width: 199px;
+  height: 42px;
+  position: absolute;
+  left: 0px;
+  top: 0px;
+  overflow: hidden;
+}
+.email-entry {
+  width: 565px;
+  height: 42px;
+  position: absolute;
+  left: 73px;
+  top: 234px;
+}
+.email-input {
+  border-style: solid;
+  border-color: #000000;
+  border-width: 1px;
+  width: 317px;
+  height: 42px;
+  position: absolute;
+  left: 248px;
+  top: 0px;
+  overflow: hidden;
+}
+.email-label {
+  border-style: solid;
+  border-color: #000000;
+  border-width: 1px;
+  width: 199px;
+  height: 42px;
+  position: absolute;
+  left: 0px;
+  top: 0px;
+  overflow: hidden;
+}
+.username-entry {
+  width: 565px;
+  height: 42px;
+  position: absolute;
+  left: 73px;
+  top: 66px;
+}
+.username-input {
+  border-style: solid;
+  border-color: #000000;
+  border-width: 1px;
+  width: 317px;
+  height: 42px;
+  position: absolute;
+  left: 248px;
+  top: 0px;
+  overflow: hidden;
+}
+.username-label {
+  border-style: solid;
+  border-color: #000000;
+  border-width: 1px;
+  width: 199px;
+  height: 42px;
+  position: absolute;
+  left: 0px;
+  top: 0px;
+  overflow: hidden;
+}
+.sign-in-options {
+  border-style: solid;
+  border-color: #000000;
+  border-width: 1px;
+  width: 281px;
+  height: 78px;
+  position: absolute;
+  left: 1159px;
+  top: 0px;
+  overflow: hidden;
+}
+.register-header {
+  border-style: solid;
+  border-color: #000000;
+  border-width: 1px;
+  width: 751px;
+  height: 78px;
+  position: absolute;
+  left: 329px;
+  top: 0px;
+  overflow: hidden;
+}
