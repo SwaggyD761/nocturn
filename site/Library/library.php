c37750e5a3aa74827050d8aa202934ff41d57290
SwaggyD761 <70922045+SwaggyD761@users.noreply.github.com>
2023-10-11 15:22:29 +0200

    Create library.php

---
+++ site/Library/library.php
@@ -0,0 +1,120 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <title>Login</title>
+  <style>
+    body {
+      background-color: steelblue;
+    }
+  </style>
+</head>
+
+<body>
+  <div class="library">
+    <div class="footer">
+      <div class="banner-ads"></div>
+      <div class="custom-notification-carousel"></div>
+      <div class="company-title"></div>
+    </div>
+    <div class="nav-bar">
+      <div class="account-icon"></div>
+      <div class="nav-links">
+        <div class="home-link-container"></div>
+        <div class="library-link-container"></div>
+        <div class="reader-link-container"></div>
+        <div class="blog-link-container"></div>
+      </div>
+      <div class="nav-header"></div>
+    </div>
+    <div class="body-container">
+      <div class="table-container">
+        <div class="table-header">
+          <div class="table-title-display"></div>
+          <div class="table-display-controls">
+            <div class="books"></div>
+            <div class="current-reads"></div>
+            <div class="notes"></div>
+          </div>
+        </div>
+        <div class="table-display"></div>
+      </div>
+      <div class="pop-up-ad-display">
+        <div class="ad-display"></div>
+      </div>
+      <div class="library-display">
+        <div class="library-header">
+          <div class="title"></div>
+          <div class="pdf-catagories">
+            <div class="all"></div>
+            <div class="recent"></div>
+            <div class="frequent"></div>
+          </div>
+        </div>
+        <div class="library-item">
+          <div class="book-image-display"></div>
+          <div class="isnb"></div>
+          <div class="author"></div>
+          <div class="title2"></div>
+        </div>
+        <div class="library-item2">
+          <div class="book-image-display"></div>
+          <div class="isnb"></div>
+          <div class="author"></div>
+          <div class="title2"></div>
+        </div>
+        <div class="library-item3">
+          <div class="book-image-display"></div>
+          <div class="isnb"></div>
+          <div class="author"></div>
+          <div class="title2"></div>
+        </div>
+        <div class="library-item4">
+          <div class="book-image-display"></div>
+          <div class="isnb"></div>
+          <div class="author"></div>
+          <div class="title2"></div>
+        </div>
+        <div class="library-item5">
+          <div class="book-image-display"></div>
+          <div class="isnb"></div>
+          <div class="author"></div>
+          <div class="title2"></div>
+        </div>
+        <div class="library-item6">
+          <div class="book-image-display"></div>
+          <div class="isnb"></div>
+          <div class="author"></div>
+          <div class="title2"></div>
+        </div>
+        <div class="library-item7">
+          <div class="book-image-display"></div>
+          <div class="isnb"></div>
+          <div class="author"></div>
+          <div class="title2"></div>
+        </div>
+        <div class="library-item8">
+          <div class="book-image-display"></div>
+          <div class="isnb"></div>
+          <div class="author"></div>
+          <div class="title2"></div>
+        </div>
+        <div class="library-item9">
+          <div class="book-image-display"></div>
+          <div class="isnb"></div>
+          <div class="author"></div>
+          <div class="title2"></div>
+        </div>
+      </div>
+      <div class="profile-display">
+        <div class="profile-imag-display"></div>
+        <div class="settings-button"></div>
+        <div class="storage-button"></div>
+        <div class="quick-information-display"></div>
+      </div>
+    </div>
+  </div>
+
+  
+</body>
+</html>
