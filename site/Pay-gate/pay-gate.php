cb2a1b63282cc62eeb4e40cd2449143650ec1ed7
SwaggyD761 <70922045+SwaggyD761@users.noreply.github.com>
2023-10-11 15:30:25 +0200

    Create pay-gate.php

---
+++ site/Pay-gate/pay-gate.php
@@ -0,0 +1,89 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <title>Login</title>
+  <style>
+    body {
+      background-color: steelblue;
+    }
+  </style>
+</head>
+
+<body>
+  <div class="pay-gate">
+    <div class="cart-check-out">
+      <div class="check-out-item">
+        <div class="image"></div>
+        <div class="index"></div>
+        <div class="title"></div>
+        <div class="print-button"></div>
+        <div class="price"></div>
+        <div class="isnb"></div>
+      </div>
+      <div class="header-display"></div>
+    </div>
+    <div class="register-card">
+      <div class="header"></div>
+      <div class="register-card-form">
+        <div class="bank-name-entry">
+          <div class="bank-name-label"></div>
+          <div class="bank-name-input"></div>
+        </div>
+        <div class="account-holders-entry">
+          <div class="account-holder-details-lable"></div>
+          <div class="account-holder-details-input"></div>
+        </div>
+        <div class="account-number-entry">
+          <div class="account-number-label"></div>
+          <div class="account-number-input"></div>
+        </div>
+        <div class="card-number-entry">
+          <div class="card-number-label"></div>
+          <div class="card-number-input"></div>
+        </div>
+        <div class="cvv-entry">
+          <div class="cvv-code-lable"></div>
+          <div class="cvv-code-input"></div>
+        </div>
+        <div class="branch-code-entry">
+          <div class="branch-code-label"></div>
+          <div class="branch-code-input"></div>
+        </div>
+        <div class="submit-button"></div>
+      </div>
+    </div>
+    <div class="pop-up-ad-display">
+      <div class="ad-display"></div>
+      <div class="close-button"></div>
+    </div>
+    <div class="check-out-confirmation">
+      <div class="header2"></div>
+      <div class="total"></div>
+      <div class="registered-card-drop-down"></div>
+      <div class="order-number"></div>
+      <div class="cart-number"></div>
+      <div class="delivery-address-textbox"></div>
+      <div class="clear-button"></div>
+      <div class="confirm-button"></div>
+    </div>
+    <div class="nav-bar">
+      <div class="account-icon"></div>
+      <div class="nav-links">
+        <div class="home-link-container"></div>
+        <div class="library-link-container"></div>
+        <div class="reader-link-container"></div>
+        <div class="blog-link-container"></div>
+      </div>
+      <div class="nav-header"></div>
+    </div>
+    <div class="footer">
+      <div class="banner-ads"></div>
+      <div class="custom-notification-carousel"></div>
+      <div class="company-title"></div>
+    </div>
+</div>
+
+  
+</body>
+</html>
