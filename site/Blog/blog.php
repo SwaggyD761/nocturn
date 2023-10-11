696fce27db9c3d98796f954c8bfcce9c8565ca20
SwaggyD761 <70922045+SwaggyD761@users.noreply.github.com>
2023-10-11 15:27:48 +0200

    Create blog.php

---
+++ site/Blog/blog.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <title>Login</title>
+  <style>
+    body {
+      background-color: steelblue;
+    }
+  </style>
+</head>
+
+<body>
+  <div class="blog">
+    <div class="blog-footer">
+      <div class="quick-info-display">
+        <div class="company-logo-display"></div>
+        <div class="company-title-display"></div>
+        <div class="email-and-location-display"></div>
+      </div>
+      <div class="contact-us">
+        <div class="submit"></div>
+        <div class="report-bug"></div>
+        <div class="clear"></div>
+        <div class="messge"></div>
+        <div class="contact-us-label"></div>
+        <div class="email-input"></div>
+        <div class="topic-input"></div>
+      </div>
+    </div>
+    <div class="body-container">
+      <div class="pop-up-ad-display">
+        <div class="ad-container"></div>
+        <div class="close-button"></div>
+      </div>
+      <div class="blog2">
+        <div class="blog-image"></div>
+        <div class="blog-text"></div>
+      </div>
+    </div>
+    <div class="nav-bar">
+      <div class="account-icon"></div>
+      <div class="nav-links">
+        <div class="home-link-container"></div>
+        <div class="library-link-container"></div>
+        <div class="reader-link-container"></div>
+        <div class="blog-link-container"></div>
+      </div>
+      <div class="nav-header"></div>
+    </div>
+</div>
+
+  
+</body>
+</html>
