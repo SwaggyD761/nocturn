3d94ab075683f21b2dea8680eecf30b0ec1b4ec6
SwaggyD761 <70922045+SwaggyD761@users.noreply.github.com>
2023-10-11 14:47:17 +0200

    Create home.php

---
+++ site/Home/home.php
@@ -0,0 +1,63 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <title>Login</title>
+  <style>
+    body {
+      background-color: steelblue;
+    }
+  </style>
+</head>
+
+<body>
+  <div class="register">
+    <div class="footer">
+      <div class="banner-ad-display"></div>
+      <div class="baner-ad-display"></div>
+      <div class="company-title"></div>
+    </div>
+    <div class="register-form-container">
+      <div class="submit-button"></div>
+      <div class="clear-button"></div>
+      <div class="register-form">
+        <div class="form-header"></div>
+        <div class="password-entry">
+          <div class="password-input"></div>
+          <div class="password-label"></div>
+        </div>
+        <div class="confirm-password-entry">
+          <div class="confirm-password-input"></div>
+          <div class="confirm-password-label"></div>
+        </div>
+        <div class="surname-entry">
+          <div class="surname-input"></div>
+          <div class="surname-label"></div>
+        </div>
+        <div class="already-have-an-account"></div>
+        <div class="register-with-google"></div>
+        <div class="cell-number-entry">
+          <div class="cell-number-input"></div>
+          <div class="cell-number-label"></div>
+        </div>
+        <div class="name-entry">
+          <div class="name-input"></div>
+          <div class="name-label"></div>
+        </div>
+        <div class="email-entry">
+          <div class="email-input"></div>
+          <div class="email-label"></div>
+        </div>
+        <div class="username-entry">
+          <div class="username-input"></div>
+          <div class="username-label"></div>
+        </div>
+      </div>
+    </div>
+    <div class="sign-in-options"></div>
+    <div class="register-header"></div>
+  </div>
+
+
+</body>
+</html>
