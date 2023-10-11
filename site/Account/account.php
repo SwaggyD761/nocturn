4434facac88949848a55661a0a65dc4432b365af
SwaggyD761 <70922045+SwaggyD761@users.noreply.github.com>
2023-10-11 15:29:02 +0200

    Create account.php

---
+++ site/Account/account.php
@@ -0,0 +1,80 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <title>Login</title>
+  <style>
+    body {
+      background-color: steelblue;
+    }
+  </style>
+</head>
+
+<body>
+  <div class="account">
+    <div class="body-container">
+      <div class="stats">
+        <div class="frame-28"></div>
+        <div class="frame-29"></div>
+      </div>
+      <div class="quick-user-info">
+        <div class="reading-and-note-stats"></div>
+        <div class="current-notes"></div>
+        <div class="current-openbooks"></div>
+      </div>
+      <div class="account-settings">
+        <div class="header"></div>
+        <div class="settings-display"></div>
+      </div>
+      <div class="app-settings">
+        <div class="header2"></div>
+        <div class="settings-display2"></div>
+      </div>
+      <div class="table-container">
+        <div class="table-controls">
+          <div class="books"></div>
+          <div class="notes"></div>
+          <div class="recent"></div>
+          <div class="favourites"></div>
+        </div>
+        <div class="table-display">
+          <div class="frame-13"></div>
+          <div class="frame-21">
+            <div class="frame-15"></div>
+            <div class="frame-17"></div>
+            <div class="frame-18"></div>
+            <div class="frame-19"></div>
+            <div class="frame-20"></div>
+            <div class="frame-16"></div>
+          </div>
+        </div>
+      </div>
+      <div class="profile">
+        <div class="frame-9">
+          <div class="frame-22"></div>
+        </div>
+        <div class="frame-23"></div>
+        <div class="frame-24"></div>
+        <div class="frame-25"></div>
+      </div>
+    </div>
+    <div class="nav-bar">
+      <div class="account-icon"></div>
+      <div class="nav-links">
+        <div class="home-link-container"></div>
+        <div class="library-link-container"></div>
+        <div class="reader-link-container"></div>
+        <div class="blog-link-container"></div>
+      </div>
+      <div class="nav-header"></div>
+    </div>
+    <div class="footer">
+      <div class="banner-ads"></div>
+      <div class="custom-notification-carousel"></div>
+      <div class="company-title"></div>
+    </div>
+</div>
+
+  
+</body>
+</html>
