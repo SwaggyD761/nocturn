6f954db20d9c136f2d7d7ec330063e35289b0b59
SwaggyD761 <70922045+SwaggyD761@users.noreply.github.com>
2023-10-11 15:19:34 +0200

    Update home.php

--- site/Home/home.php
+++ site/Home/home.php
@@ -11,51 +11,44 @@
 </head>
 
 <body>
-  <div class="register">
-    <div class="footer">
-      <div class="banner-ad-display"></div>
-      <div class="baner-ad-display"></div>
-      <div class="company-title"></div>
+  <div class="home">
+    <div class="nav-bar">
+      <div class="account-icon"></div>
+      <div class="nav-links">
+        <div class="home-link-container"></div>
+        <div class="library-link-container"></div>
+        <div class="reader-link-container"></div>
+        <div class="blog-link-container"></div>
+      </div>
+      <div class="nav-header"></div>
     </div>
-    <div class="register-form-container">
-      <div class="submit-button"></div>
-      <div class="clear-button"></div>
-      <div class="register-form">
-        <div class="form-header"></div>
-        <div class="password-entry">
-          <div class="password-input"></div>
-          <div class="password-label"></div>
+    <div class="body-contaiber">
+      <div class="profile-display">
+        <div class="profile-imag-display"></div>
+        <div class="settings-button"></div>
+        <div class="storage-button"></div>
+        <div class="quick-information-display"></div>
+      </div>
+      <div class="notfications-display">
+        <div class="hearder">
+          <div class="storage-indicator"></div>
+          <div class="notifications-heading"></div>
         </div>
-        <div class="confirm-password-entry">
-          <div class="confirm-password-input"></div>
-          <div class="confirm-password-label"></div>
-        </div>
-        <div class="surname-entry">
-          <div class="surname-input"></div>
-          <div class="surname-label"></div>
-        </div>
-        <div class="already-have-an-account"></div>
-        <div class="register-with-google"></div>
-        <div class="cell-number-entry">
-          <div class="cell-number-input"></div>
-          <div class="cell-number-label"></div>
-        </div>
-        <div class="name-entry">
-          <div class="name-input"></div>
-          <div class="name-label"></div>
-        </div>
-        <div class="email-entry">
-          <div class="email-input"></div>
-          <div class="email-label"></div>
-        </div>
-        <div class="username-entry">
-          <div class="username-input"></div>
-          <div class="username-label"></div>
-        </div>
+        <div class="notification-display"></div>
+        <div class="banner-ads-display"></div>
+      </div>
+      <div class="ad-and-logo-carousel"></div>
+      <div class="quick-acces-buttons">
+        <div class="reader-button-container"></div>
+        <div class="library-button-container"></div>
+        <div class="blog-button-container"></div>
       </div>
     </div>
-    <div class="sign-in-options"></div>
-    <div class="register-header"></div>
+    <div class="footer">
+      <div class="banner-ads"></div>
+      <div class="custom-notification-carousel"></div>
+      <div class="company-title"></div>
+    </div>
   </div>
 
 
