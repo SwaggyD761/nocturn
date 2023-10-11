6dc3d9f458234f1a8b2abb4eff8eda1399656015
SwaggyD761 <70922045+SwaggyD761@users.noreply.github.com>
2023-10-11 15:16:59 +0200

    Create login.php

---
+++ login.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <title>Login</title>
+  <style>
+    body {
+      background-color: steelblue;
+    }
+  </style>
+</head>
+
+<body>
+  <div class="login">
+    <div class="login-form-container">
+      <div class="submit-button"></div>
+      <div class="register-button"></div>
+      <div class="login-form">
+        <div class="form-header"></div>
+        <div class="sign-in-with-google"></div>
+        <div class="forgot-password"></div>
+        <div class="email-entry">
+          <div class="email-input"></div>
+          <div class="email-label"></div>
+        </div>
+        <div class="password-entry">
+          <div class="password-input"></div>
+          <div class="password-label"></div>
+        </div>
+        <div class="username-entry">
+          <div class="username-input"></div>
+          <div class="username-label"></div>
+        </div>
+      </div>
+    </div>
+    <div class="login-option"></div>
+    <div class="login-header"></div>
+    <div class="footer">
+      <div class="banner-ad-display"></div>
+      <div class="baner-ad-display"></div>
+      <div class="company-title"></div>
+    </div>
+  </div>
+  
+</body>
+</html>
+
+
+
+
+
+
+
+
+
