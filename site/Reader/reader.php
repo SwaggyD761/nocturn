683032de3b463c05b5d102f5895aae6c3e491ead
SwaggyD761 <70922045+SwaggyD761@users.noreply.github.com>
2023-10-11 15:43:21 +0200

    Create reader.php

---
+++ site/Reader/reader.php
@@ -0,0 +1,71 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <title>Login</title>
+  <style>
+    body {
+      background-color: steelblue;
+    }
+  </style>
+</head>
+
+<body>
+  <div class="body-container">
+    <div class="pop-up-ad">
+      <div class="header">
+        <div class="close-button">
+          <div class="frame-100"></div>
+        </div>
+      </div>
+      <div class="dsiplay"></div>
+    </div>
+    <div class="text-box">
+      <div class="tool-bar">
+        <div class="clear-button"></div>
+        <div class="submit-button"></div>
+      </div>
+      <div class="text-display"></div>
+    </div>
+    <div class="pdf-reader">
+      <div class="reader-container">
+        <div class="text-display2"></div>
+        <div class="reader-header">
+          <div class="compile-notes"></div>
+          <div class="header2">
+            <div class="text-title-display"></div>
+            <div class="dropdown-menut"></div>
+          </div>
+        </div>
+      </div>
+      <div class="reader-controls">
+        <div class="bookmark-button"></div>
+        <div class="highlight-text-button"></div>
+        <div class="add-notes-button"></div>
+        <div class="read-button"></div>
+        <div class="save-changes-button"></div>
+      </div>
+    </div>
+    <div class="place-holder"></div>
+    <div class="profile-display">
+      <div class="profile-imag-display"></div>
+      <div class="settings-button"></div>
+      <div class="storage-button"></div>
+      <div class="quick-information-display"></div>
+    </div>
+    <div class="table-container">
+      <div class="table-header">
+        <div class="table-title-display"></div>
+        <div class="table-display-controls">
+          <div class="notes"></div>
+          <div class="note-groups"></div>
+          <div class="notes2"></div>
+        </div>
+      </div>
+      <div class="table-display"></div>
+    </div>
+  </div>
+
+  
+</body>
+</html>
